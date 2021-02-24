8e05da23d811e48b17f24da9dafe86ed98c6258b
Berto <Berto@DESKTOP-6V1E3CK>
2021-02-24 22:21:39 +0100

    Cambios

---
+++ src/github/prueba/GitHubPRUEBA.java
@@ -0,0 +1,15 @@
+
+package github.prueba;
+
+/**
+ *
+ * @author Berto
+ */
+public class GitHubPRUEBA {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo. Como estamos");
+    }
+    
+}
